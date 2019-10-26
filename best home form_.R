7853eff9b2140b492c0403e8fd41a5c26498adb2
Michael O'Shea <osheam98@outlook.com>
2019-10-26 16:43:13 +1000

    Arsenal home vs away goal diff

--- best home form_.R
+++ best home form_.R
@@ -9,3 +9,10 @@ library(magrittr)
 #what does it look like?
 summary(e18)
 head(e18)
+#how does Arsenal broadly fare
+ars_h <- filter(e18, HomeTeam=="Arsenal") 
+ars_a <- filter(e18, AwayTeam=="Arsenal") 
+#how many more goals to arsenal score than their opposition at home
+sum (ars_h$FTHG - ars_h$FTAG) #26 more
+  #and away?
+sum (ars_a$FTAG - ars_a$FTHG) #4 less
