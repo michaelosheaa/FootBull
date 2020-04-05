7c06ca61a91b4881dc32c660c2de918956b85510
Michael O'Shea <osheam98@outlook.com>
2020-04-05 21:11:10 +1000

    splitting setup from exploration

--- exploration.R
+++ exploration.R
@@ -1,9 +1,3 @@
-setwd("~/GitHub/FootBull")
-#load epl 18/19 file
-e18<- read.csv("epl_season_1819.csv")
-#libraries
-library(tidyverse)
-library(magrittr)
 
 #begin
 #what does it look like?
@@ -12,7 +6,7 @@ head(e18)
 #how does Arsenal broadly fare
 ars_h <- filter(e18, HomeTeam=="Arsenal") 
 ars_a <- filter(e18, AwayTeam=="Arsenal") 
-#how many more goals to arsenal score than their opposition at home
+#how many more goals do arsenal score than their opposition at home
 sum (ars_h$FTHG - ars_h$FTAG) #26 more
   #and away?
 sum (ars_a$FTAG - ars_a$FTHG) #4 less
@@ -23,3 +17,6 @@ e18_no_g <- select(e18, Div:AR)
 #only home variables
 e18_h <- select(e18, starts_with('H'), -HTAG, Referee, FTR)
 colnames(e18_h)
+
+unique(e18$Referee)
+summary(e18$)
---
+++ setup.R
@@ -0,0 +1,12 @@
+#
+#This file loads packages and datasets for EPL exploration
+#
+#Michael O'Shea 5/4/20
+
+setwd("~/GitHub/FootBull")
+#load epl 18/19 file
+e18<- read.csv("epl_season_1819.csv")
+#libraries
+library(tidyverse)
+library(magrittr)
+library(gitignore)
